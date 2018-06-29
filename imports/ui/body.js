54836f392096155ae8e1e79c0966e80da3494aad
Samuel Caetité <sscaetite@gmail.com>
2018-06-29 11:46:09 -0300

    Adicionado botão de inscrever equipe

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,40 @@ Template.body.helpers({
     return Equipes.find({});
   },
 });
+
+Template.body.events({
+  'submit .inscEquipe'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const nome = target.nome.value;
+    const capitao = target.capitao.value;
+    const top = target.top.value;
+    const jungler = target.jungler.value;
+    const mid = target.mid.value;
+    const adc = target.adc.value;
+    const supp = target.supp.value;
+
+    Equipes.insert(
+      {
+        nome,
+        capitao,
+        top,
+        jungler,
+        mid,
+        adc,
+        supp,
+        createdAt: new Date()
+      },
+    );
+
+    target.nome.value = '';
+    target.capitao.value = '';
+    target.top.value = '';
+    target.jungler.value = '';
+    target.mid.value = '';
+    target.adc.value = '';
+    target.supp.value = '';
+    
+  },
+});
