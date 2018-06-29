143810d4df5152f0dfa634e13f1753a0483031d6
Samuel Caetité <sscaetite@gmail.com>
2018-06-28 23:52:09 -0300

    Adicionado flexbox às tabelas de equipes

--- client/main.js
+++ client/main.js
@@ -25,6 +25,16 @@ Template.body.helpers({
 		},
 
 		{
+			nome: 'zHao123',
+			capitao: 'Capitâo Br',
+			top: 'i4D DragonKame',
+			jungler: 'Monux Zyrow',
+			mid: 'Capitâo Br',
+			adc: 'Lord Zorg',
+			supp: 'MasterHamlet'
+		},
+
+		{
 			nome: '7 Pecados',
 			capitao: 'Gula',
 			top: 'Preguiça',
